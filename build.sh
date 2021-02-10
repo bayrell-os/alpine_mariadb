0bdfb6471b10996ed40305a4fb56581bd4de55cd
Ildar Bikmamatov <ildar@bayrell.org>
2021-02-10 18:40:27 +0000

    add arm64

--- build.sh
+++ build.sh
@@ -15,6 +15,10 @@ case "$1" in
 		docker build ./ -t bayrell/alpine_mariadb:$VERSION-$SUBVERSION-amd64 --file Dockerfile --build-arg ARCH=-amd64
 	;;
 	
+	arm64v8)
+		docker build ./ -t bayrell/alpine_mariadb:$VERSION-$SUBVERSION-arm64v8 --file Dockerfile --build-arg ARCH=-arm64v8
+	;;
+	
 	arm32v7)
 		docker build ./ -t bayrell/alpine_mariadb:$VERSION-$SUBVERSION-arm32v7 --file Dockerfile --build-arg ARCH=-arm32v7
 	;;
@@ -23,27 +27,31 @@ case "$1" in
 		rm -rf ~/.docker/manifests/docker.io_bayrell_alpine_mariadb-*
 		
 		docker push bayrell/alpine_mariadb:$VERSION-$SUBVERSION-amd64
+		docker push bayrell/alpine_mariadb:$VERSION-$SUBVERSION-arm64v8
 		docker push bayrell/alpine_mariadb:$VERSION-$SUBVERSION-arm32v7
 		
 		docker manifest create --amend bayrell/alpine_mariadb:$VERSION-$SUBVERSION \
 			bayrell/alpine_mariadb:$VERSION-$SUBVERSION-amd64 \
+			bayrell/alpine_mariadb:$VERSION-$SUBVERSION-arm64v8 \
 			bayrell/alpine_mariadb:$VERSION-$SUBVERSION-arm32v7
 		docker manifest push --purge bayrell/alpine_mariadb:$VERSION-$SUBVERSION
 		
 		docker manifest create --amend bayrell/alpine_mariadb:$VERSION \
 			bayrell/alpine_mariadb:$VERSION-$SUBVERSION-amd64 \
+			bayrell/alpine_mariadb:$VERSION-$SUBVERSION-arm64v8 \
 			bayrell/alpine_mariadb:$VERSION-$SUBVERSION-arm32v7
 		docker manifest push --purge bayrell/alpine_mariadb:$VERSION
 	;;
 	
 	all)
 		$0 amd64
+		$0 arm64v8
 		$0 arm32v7
 		$0 manifest
 	;;
 	
 	*)
-		echo "Usage: $0 {amd64|arm32v7|manifest|all}"
+		echo "Usage: $0 {amd64|arm64v8|arm32v7|manifest|all}"
 		RETVAL=1
 
 esac
